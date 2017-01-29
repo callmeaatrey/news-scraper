acc96438fe01f3c17f10b73b7427bc3ffa6bbdec
Shikher Aatrey <shikher.aatrey@juspay.in>
2017-01-29 23:57:58 +0530

    Initial release: for getting markup

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# news-scraper
+News Scraper
+=========
+
 Fast, lean implementation of a web scraper for gaining access to news from various news centers all around the world.
+
+## Installation
+
+  `npm install news-scraper --save`
+
+## Usage
+
+    var scraper = require('news-scraper');
+
+    var formattedNum = scraper('techcrunch', ['link to a post', 'another link to a post']);
+
+
+  Output should be `{'result': 'json object containing blog post'}`
+
+
+## Contributors
+
+In lieu of a formal style guide, take care to maintain the existing coding style. Lint and test your code.
---
+++ index.js
@@ -0,0 +1,34 @@
+'use-strict';
+
+/**
+ * Scraper for gaining access to news from news centers all over the world
+ * @param {string} [source] [news source name]
+ * @param {url:string} [link] [link to be scraped]
+ * @returns {object} [blog post in a json format]
+ */
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+var sources = ['techcrunch'];
+
+module.exports = function(source, link) {
+	if sources.indexOf(source) == -1
+		return {'body': 'sorry! support not available yet for the required source'};
+
+	var scrape_map = {
+		"techcrunch": techcrunch
+	};
+
+	scrape_map[source](link);
+}
+
+// scraper for techcrunch posts
+function techcrunch(link) {
+	request(link, function(err, res, html) {
+		if (!err) {
+			var $ = cheerio.load(html);
+			console.log($);
+		}
+	})
+}
