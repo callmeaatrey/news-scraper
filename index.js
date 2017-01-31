0298a8a509268283d17a68851b759437a291b6cf
Shikher Aatrey <shikher.aatrey@juspay.in>
2017-01-31 13:23:51 +0530

    Callback functionality added for handling success/error reports

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ var async = require('async');
 
 var sources = ['techcrunch'];
 
-module.exports = function(source, link) {
+module.exports = function(source, link, callback) {
+	var json_post = [];
 	if (sources.indexOf(source) == -1)
 		return {'body': 'sorry! support not available yet for the required source'};
 
@@ -21,38 +22,35 @@ module.exports = function(source, link) {
 		"techcrunch": techcrunch
 	};
 
-	scrape_map[source](link);
-}
+	var blog_json = scrape_map[source](link);
 
-// scraper for techcrunch posts
-function techcrunch(link) {
-	// for handling incoming link array
-	if (Array.isArray(link)) {
-		async.map(link, function(url, callback) {
-			request(url, function(err, res, html) {
-				if(!err) {
-					var $ = JSON.parse(html);
-					callback(null, $);
-				} else {
-					callback(err || res.statusCode);
+	// scraper for techcrunch posts
+	function techcrunch(link) {
+		// for handling incoming link array
+		if (Array.isArray(link)) {
+			console.log('Techcrunch scraper started!');
+			async.map(link, function(url, next) {
+				request(url, function(err, res, html) {
+					if(!err) {
+						json_post.push({url: url});
+						next();
+					} else {
+						// handle error here
+					}
+				})
+			}, function() {
+					console.log(json_post);
+					return callback(json_post);
+			});
+		}
+		// for handling incoming single link
+		else {
+			request(link, function(err, res, html) {
+				if (!err) {
+					var $ = cheerio.load(html);
+					console.log($);
 				}
 			})
-		}, function(err, result) {
-			if(!err) {
-				console.log(result);
-			}
-			else {
-				// handle error here
-			}
-		});
-	}
-	// for handling incoming single link
-	else {
-		request(link, function(err, res, html) {
-			if (!err) {
-				var $ = cheerio.load(html);
-				console.log($);
-			}
-		})
+		}
 	}
 }
